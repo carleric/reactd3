6681efd96b496c219dc8ad869c4ba2c17dac9fd9
Carl Bolstad <cbolstad@gmail.com>
2016-01-24 10:55:11 -0800

    fix data retrieval

--- src/index.jsx
+++ src/index.jsx
@@ -18,21 +18,21 @@ var H1BGraph = React.createClass({
         debugger;
         var dateFormat = d3.time.format("%m/%d/%Y");
         d3.csv(this.props.url)
-            .row(function(d){
+          .row(function(d){
                 if(!d['base salary']) {
                     return null;
                 }
                 return {employer: d.employer,
                     submit_date: dateFormat.parse(d['submit date']),
-                    start_date: dateFormat.parse(d['start_date']),
-                    case_status: d['case_status'],
-                    job_title: d['job_title'],
-                    base_salary: Number(d['base_salary']),
-                    salary_to: d['salary_to'] ? Number(d['salary to']) : null,
+                    start_date: dateFormat.parse(d['start date']),
+                    case_status: d['case status'],
+                    job_title: d['job title'],
+                    base_salary: Number(d['base salary']),
+                    salary_to: d['salary to'] ? Number(d['salary to']) : null,
                     city: d.city,
                     state: d.state};
             }.bind(this))
-            .get(function(error, rows) {
+          .get(function(error, rows) {
                 if(error) {
                     console.error(error);
                     console.error(error.stack);
@@ -72,6 +72,6 @@ var H1BGraph = React.createClass({
 });
 
 ReactDOM.render (
-    <H1BGraph url="./data/h1bs.csv" />,
+    <H1BGraph url="data/h1bs.csv" />,
     document.querySelectorAll('.h1bgraph')[0]
     );
